b907cd4a931d4e5a580b35c6da164eb93f3a804e
Jeremy OBryan <jsobryan@gmail.com>
2021-03-13 20:39:01 -0600

    initial project template

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_BASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+class Shopping(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+class Menu(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    item = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+
+@app.route('/')
+def index():
+    now = datetime.datetime.now() # current date and time
+    date_time = now.strftime("%A, %B %W %Y")
+    #show all todos
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list, date_time = date_time)
+
+     
+
+@app.route('/add', methods=["POST"])
+def add():
+    #add new item
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route('/update/<int:todo_id>')
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+@app.route('/delete/<int:todo_id>')
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    db.create_all()
+
+    new_todo = Todo(title="todo 1", complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    app.run(debug=True, port=5000, host='0.0.0.0')
